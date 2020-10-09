73b525da36cfe85c48ecf701327c4a24e4e51e1f
Bento Borges Schirmer <bento.schirmer@gmail.com>
2020-10-09 15:02:41 -0300

    Something added

---
+++ server/more-bs.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+
+int main(int argc, char *argv[])
+{
+	printf("Hello world\n\n\n\n\n\n");
+	return 0;
+}
